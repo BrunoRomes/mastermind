2d6e90f0ef20c56061dd000ff9805446257d2d4d
Bruno Lima <brunoromes@gmail.com>
2016-05-20 23:22:17 -0300

    Downgrading to rails 4.2.6

--- spec/integration/templates_spec.rb
+++ spec/integration/templates_spec.rb
@@ -6,7 +6,7 @@ describe "Templates api" do
 
     it "returns an array with numbers from 0 to 9" do
       get "/templates", {}, { "Accept" => "application/json", "Content-Type" => "application/json"}
-      expect(response.status).to eq 200
+      expect(response).to have_http_status(200)
       json = JSON.parse(response.body)
       expect(json.size).to eq 10
     end
